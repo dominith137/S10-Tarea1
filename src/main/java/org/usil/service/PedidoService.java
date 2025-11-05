8c51f457697b3c10b6060219e3472e9ce186697d
dominith <dominith456@gmail.com>
2025-11-05 16:23:11 -0500

    Implementado clases de service

---
+++ src/main/java/org/usil/service/PedidoService.java
@@ -0,0 +1,24 @@
+package org.usil.service;
+
+import org.usil.model.Pedido;
+import org.usil.model.Producto;
+
+public class PedidoService {
+    
+    private static int contadorPedidos = 1;
+
+    public boolean registrarPedido(Pedido pedido) {
+        // Actualizar el stock del producto
+        Producto producto = pedido.getProducto();
+        int stockActual = producto.getStock();
+        int cantidadPedida = pedido.getCantidad();
+        producto.setStock(stockActual - cantidadPedida);
+
+        return true;
+    }
+    
+    public String generarNumeroPedido() {
+        return "PED-" + String.format("%06d", contadorPedidos++);
+    }
+}
+
